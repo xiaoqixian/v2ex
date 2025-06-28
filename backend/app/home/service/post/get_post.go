de7ff13e58035a98cc5863ec04ca8408816c2707
lunar <lunar_ubuntu@qq.com>
2025-06-28 23:26:51 +0800

    GetPost handler

--- backend/app/home/service/post/get_post.go
+++ backend/app/home/service/post/get_post.go
@@ -7,12 +7,14 @@ package post_service
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/xiaoqixian/v2ex/backend/app/home/util"
 	"github.com/xiaoqixian/v2ex/backend/rpc_gen/postpb"
+	"github.com/xiaoqixian/v2ex/backend/rpc_gen/userpb"
 )
 
 func GetPost(ginCtx *gin.Context) {
@@ -24,12 +26,27 @@ func GetPost(ginCtx *gin.Context) {
 		return
 	}
 
-	req := postpb.GetPostRequest {
-		PostId: uint64(postID),
+	var userID uint
+	accessToken, err := ginCtx.Cookie("access_token")
+	log.Println("access_token detected in cookie, get post with user id")
+	if err == nil {
+		parse := func(ctx context.Context, client userpb.UserServiceClient) {
+			resp, err2 := client.AuthMe(ctx, &userpb.AuthMeRequest {
+				AccessToken: accessToken,
+			})
+			if err2 == nil && resp.Success {
+				userID = uint(resp.UserId)
+				log.Printf("successfully get user id %d\n", userID)
+			}
+		}
+		util.WithRPCClient("localhost:8081", userpb.NewUserServiceClient, parse)
 	}
 
 	callback := func(ctx context.Context, client postpb.PostServiceClient) {
-		resp, err2 := client.GetPost(ctx, &req)
+		resp, err2 := client.GetPost(ctx, &postpb.GetPostRequest {
+			PostId: uint64(postID),
+			UserId: uint64(userID),
+		})
 		if err2 != nil {
 			ginCtx.JSON(http.StatusServiceUnavailable, gin.H {
 				"error": fmt.Sprintf("RPC error: %s", err2.Error()),
