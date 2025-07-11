6f58724313c2408859c82a52f7c5a488e8d6670a
lunar <lunar_ubuntu@qq.com>
2025-07-11 12:27:33 +0800

    Remove JWT authentication module from user service

--- backend/app/user/service/user_service.go
+++ backend/app/user/service/user_service.go
@@ -7,12 +7,9 @@ package service
 import (
 	"context"
 	"log"
-	"time"
 
 	// "github.com/xiaoqixian/v2ex/backend/app/user/conf"
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/redis/go-redis/v9"
-	"github.com/xiaoqixian/v2ex/backend/app/user/conf"
 	"github.com/xiaoqixian/v2ex/backend/app/user/dal"
 	"github.com/xiaoqixian/v2ex/backend/app/user/model"
 	"github.com/xiaoqixian/v2ex/backend/rpc_gen/userpb"
@@ -103,74 +100,25 @@ func (impl *UserServiceImpl) Login(
 		}, nil
 	}
 
-	c := conf.GetConf()
-	accessToken, err := generateToken(user.ID, time.Minute * time.Duration(c.Jwt.AccExpTime))
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(in.Password), bcrypt.DefaultCost)
 	if err != nil {
-		log.Printf("generateToken error: %s\n", err.Error())
+		log.Fatalln(err.Error())
+	}
+	
+	if string(hashedPassword) != user.PasswordHashed {
 		return &userpb.LoginResponse {
-			Success: true,
-			Message: "登录成功",
+			Success: false,
+			Message: "密码错误",
 		}, nil
 	}
 
-	refreshToken, err := generateToken(user.ID, time.Minute * time.Duration(c.Jwt.RefExpTime))
-	if err != nil {
-		log.Printf("generateToken error: %s\n", err.Error())
-		return &userpb.LoginResponse {
-			Success: true,
-			Message: "登录成功",
-		}, nil
-	}
-	
 	log.Printf("User(%d, '%s') logged in\n", user.ID, user.Username)
 	return &userpb.LoginResponse {
 		Success: true,
 		Message: "登录成功",
-		AccessToken: accessToken,
-		RefreshToken: refreshToken,
-		AccessExpiresIn: int64(c.Jwt.AccExpTime),
-		RefreshExpiresIn: int64(c.Jwt.RefExpTime),
 		User: &userpb.LoginResponse_User {
 			Id: uint64(user.ID),
 		},
-	}, nil
-}
-
-func (impl *UserServiceImpl) RefreshToken(
-	ctx context.Context,
-	in *userpb.RefreshTokenRequest,
-) (*userpb.RefreshTokenResponse, error) {
-	userID, valid := parseToken(in.RefreshToken)
-	if !valid {
-		// leave accessToken empty to represent the token is invalid
-		return &userpb.RefreshTokenResponse {}, nil
-	}
-
-	if user := model.GetUserInfoById(impl.db, ctx, userID); user == nil {
-		return &userpb.RefreshTokenResponse {}, nil
-	}
-	
-	c := conf.GetConf()
-	accessToken, err := generateToken(userID, time.Minute * time.Duration(c.Jwt.AccExpTime))
-	if err != nil {
-		log.Fatalf("generate jwt token error: %s\n", err.Error())
-		return &userpb.RefreshTokenResponse {}, nil
-	}
-	return &userpb.RefreshTokenResponse {
-		AccessToken: accessToken,
-		ExpiresIn: int64(c.Jwt.AccExpTime),
-		TokenType: "Bearer",
-	}, nil
-}
-
-func (impl *UserServiceImpl) AuthMe(
-	ctx context.Context,
-	in *userpb.AuthMeRequest,
-) (*userpb.AuthMeResponse, error) {
-	userID, valid := parseToken(in.AccessToken)
-	return &userpb.AuthMeResponse {
-		Success: valid,
-		UserId: uint64(userID),
 	}, nil
 }
 
@@ -215,37 +163,4 @@ func (impl *UserServiceImpl) GetBatchUserInfo(
 	return &userpb.GetBatchUserInfoResponse {
 		UserInfoList: userInfoList,
 	}, nil
-}
-
-func generateToken(userID uint, d time.Duration) (string, error) {
-	c := conf.GetConf()
-	claims := jwt.MapClaims {
-		"user_id": userID,
-		"exp": time.Now().Add(d).Unix(),
-		"iat": time.Now().Unix(),
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(c.Jwt.Secret))
-}
-
-func parseToken(tokenString string) (userid uint, valid bool) {
-	c := conf.GetConf()
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
-		return []byte(c.Jwt.Secret), nil
-	})
-
-	if err != nil {
-		log.Printf("jwt token parse error: %s\n", err.Error())
-		return 0, false
-	}
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		userIDFloat, ok := claims["user_id"].(float64)
-		if !ok {
-			return 0, false
-		}
-		return uint(userIDFloat), true
-	}
-	return 0, false
 }
